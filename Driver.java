4ba5fe91ec226c571e7738e46b8498142d4ef69a
Mohammad-Agha <hamadaagha123@gmail.com>
2021-01-31 17:40:05 +0200

    Added more functions to the array implementation with javadoc

--- Driver.java
+++ Driver.java
@@ -1,16 +1,8 @@
 import customlib.MVector;
 
-public class Driver
-{
-  public static void main(String[] args)
-  {
+public class Driver {
+  public static void main(String[] args) {
     MVector array = new MVector();
-    for(int i = 0; i < 40; i++)
-    {
-      array.add(i + 1);
-    }
-    int value = array.remove();
-    System.out.println(value);
-    array.print();
+
   }
 }
