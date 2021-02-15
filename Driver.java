b533904a6903cb2a01b8f69e87ceb9906233509e
Mohammad-Agha <hamadaagha123@gmail.com>
2021-02-15 21:48:35 +0200

    Binary search tree implementation with some functions

--- Driver.java
+++ Driver.java
@@ -1,8 +1,17 @@
-import customlib.MVector;
+import java.util.ArrayList;
+
+import customlib.BinaryTree;
 
 public class Driver {
   public static void main(String[] args) {
-    MVector array = new MVector();
+    BinaryTree tree = new BinaryTree();
+    tree.insert(11);
+    tree.insert(5);
+    tree.insert(12);
+    tree.insert(4);
+    tree.insert(6);
+    tree.insert(10);
 
+    tree.printPostOrder();
   }
 }
