4ecbe24ea1445552b2feb4bb4c33bab3f3225f32
Mohammad-Agha <hamadaagha123@gmail.com>
2021-02-20 17:43:01 +0200

    Linked list implementation with generics

--- Driver.java
+++ Driver.java
@@ -1,17 +1,3 @@
-import java.util.ArrayList;
-
-import customlib.BinaryTree;
-
 public class Driver {
-  public static void main(String[] args) {
-    BinaryTree tree = new BinaryTree();
-    tree.insert(11);
-    tree.insert(5);
-    tree.insert(12);
-    tree.insert(4);
-    tree.insert(6);
-    tree.insert(10);
 
-    tree.printPostOrder();
-  }
 }
